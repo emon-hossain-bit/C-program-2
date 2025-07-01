ec9575c56620ae953d3560ad385ce4cc06aba3eb
emon-hossain-bit <emon6626070@gmail.com>
2025-07-01 21:26:58 +0600

    Editing file hello23.c

--- Hello23.c
+++ Hello23.c
@@ -1,15 +1,13 @@
 #include<stdio.h>
 #include<math.h>
-void printhello();
-void Goodbye();
-void Salam();
-void Bonjour();
-int sum(int a, int b);
-void table(int n);
-void calprice(float value );
+
 int main(){
-    int n=4;
-    printf("%f",pow(n,2));
+    float x;
+    float pi;
+    pi=acos(-1);
+    printf("Enter the radius: ");
+    scanf("%f",&x);
+    printf("The area of the circle is: %.2f",x*x*pi);
     
     
 
@@ -17,33 +15,3 @@ int main(){
 }
 
 
-void printhello(){
-    printf("Hello\n");
-}
-
-void Goodbye(){
-    printf("Good bye\n");
-}
-
-void Salam(){
-    printf("Assalamo alaikom\n");
-}
-
-void Bonjour(){
-    printf("Bonjour\n");
-}
-
-int sum(int a, int b){
-    return a+b;
-}
-
-void table(int n){
-    for(int i=1;i<=10;i++){
-        printf("%d\n",n*i);
-    }
-}
-
-void calprice(float value){
-    value=value + (0.18*value);
-    printf("Final price is:%f",value);
-}
