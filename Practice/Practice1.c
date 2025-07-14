2dbecef25d6872ea6795aa2bf871849fa5982f3b
emon-hossain-bit <emon6626070@gmail.com>
2025-07-14 11:02:43 +0600

    Adding Practice folder

---
+++ Practice/Practice1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/*Write a program to print missing digite from serial integar number in an array.*/
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i=1;i<n;i++){
+        if(arr[i]!=arr[i-1]+1){
+            printf("%d\n",arr[i]-1);
+        }
+    }
+	return 0;
+}
