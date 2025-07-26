6e0e00f845516d7c5b69d30044aff38b05d77173
emon-hossain-bit <emon6626070@gmail.com>
2025-07-26 11:24:04 +0600

    Modifay Pactice1 file

--- Practice/Practice1.c
+++ Practice/Practice1.c
@@ -16,3 +16,32 @@ int main(){
     }
 	return 0;
 }
+
+
+
+// int main(){
+//     int n;
+//     printf("Enter the num: ");
+//     scanf("%d",&n);
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         scanf("%d",&arr[i]);
+//     }
+//     for(int i=1;i<n;i++){
+//         if(arr[i]-arr[i-1]==2){
+//             if(arr[i]!=arr[i-1]+1){
+//                printf("%d\n",arr[i]-1);
+//             }
+//         }
+//         else if(arr[i]-arr[i-1]>2){
+//             int d;
+//             d=arr[i]-arr[i-1];
+//             for(int j=1;j<d;j++){
+//                 arr[i-1]++;
+//                 printf("%d\n",arr[i-1]);
+//             }
+//         }
+//     }
+
+//     return 0;
+// }
