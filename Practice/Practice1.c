13c119439180684fe9fc08cf0f6081745ac6d0c8
emon-hossain-bit <emon6626070@gmail.com>
2025-08-05 15:15:29 +0600

    Editing Practice1 file

--- Practice/Practice1.c
+++ Practice/Practice1.c
@@ -1,21 +1,49 @@
 #include<stdio.h>
 
-/*Write a program to print missing digite from serial integar number in an array.*/
-
-int main(){
-    int n;
-    scanf("%d",&n);
-    int arr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+/*Write a program to print missing digite from serial integar number in an array.
+Input will be till EOF.*/
+int main () {
+    int arr[100],count=0;
+    int i=0;
+    while(scanf("%d",&arr[i])!=EOF){
+        count++;
+        i++;
     }
-    for(int i=1;i<n;i++){
-        if(arr[i]!=arr[i-1]+1){
-            printf("%d\n",arr[i]-1);
+    for(int i=1;i<=count;i++){
+        if(arr[i]-arr[i-1]==2){
+            if(arr[i]!=arr[i-1]+1){
+                printf("%d\n",arr[i]-1);
+            }
         }
+        else if(arr[i]-arr[i-1]>2){
+            int d;
+            d=arr[i]-arr[i-1];
+            for(int j=1;j<d;j++){
+                arr[i-1]++;
+                printf("%d\n",arr[i-1]);
+            }
+        }
+
     }
-	return 0;
+    return 0;
 }
+
+
+
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         scanf("%d",&arr[i]);
+//     }
+//     for(int i=1;i<n;i++){
+//         if(arr[i]!=arr[i-1]+1){
+//             printf("%d\n",arr[i]-1);
+//         }
+//     }
+// 	return 0;
+// }
 
 
 
